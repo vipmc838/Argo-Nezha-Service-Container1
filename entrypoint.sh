6e253878cb8dc873e95ab728c966deb766fbb816
vipmc838 <vipmc838@gmail.com>
2024-09-15 20:45:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-apk add --no-cache vim
-
+sudo apt 
 
 # 如参数不齐全，容器退出
 [[ -z "$WEB_DOMAIN" || -z "$DATA_DOMAIN" ]] && echo " Variables of WEB_JSON and SERVER_JSON Variables are required. " && exit 1
